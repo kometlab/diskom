35725a510fe564fcaadfbe7c0a4ce7ef294966e3
Komet <126344636+kometlab@users.noreply.github.com>
2023-04-17 17:33:10 +0200

    Initial commit

---
+++ website.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    with open('database/db.json', 'r') as file:
+        data = json.load(file)
+    if request.method == 'POST':
+        data1 = request.json
+        data.append(dict(data1))
+        print(data)
+        with open('database/db.json', 'w') as file2:
+            json.dump(
+                data,
+                file2,
+                indent=4
+            )
+    return render_template('index.html', data=data)
